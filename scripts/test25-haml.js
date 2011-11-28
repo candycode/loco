6aeb5f2b51a32fb90e573d6f551b6f30c85882f5
ugo <ugovaretto@gmail.com>
2011-11-29 00:12:38 +0100

    Added haml.js and samples

---
+++ scripts/test25-haml.js
@@ -0,0 +1,41 @@
+try {
+var include = Loco.ctx.include,
+    print = Loco.console.println,
+    readline = Loco.console.readLine,
+    fread = Loco.fs.fread,
+    fwrite = Loco.fs.fwrite,
+    exit = Loco.ctx.exit,
+    ctx = Loco.ctx,
+    quit = Loco.ctx.quit,
+    HOMEDIR = Loco.ctx.homeDir(),
+    APPDIR = Loco.ctx.appDir(),
+    CURDIR = Loco.ctx.curDir(),
+    WEBKIT = Loco.ctx.jsInterpreterName().indexOf( "webkit" ) >= 0, 
+    err = function(msg) { throw msg; },
+    CONTEXT_TYPE = WEBKIT ? "JavaScriptCoreContext" : "QtScriptContext";
+
+Loco.ctx.onError.connect( function( msg ) { print( msg ); } );
+Loco.ctx.javaScriptConsoleMessage.connect(
+ function( msg, line, src ) {
+   print( msg + " at line " + line );
+ } );
+//==============================================================================
+include(":/json2.js");
+include("../filters/haml.js");
+print_date = function () { return "today";};
+var current_user = { address: "my address", email: "e@ma.il", bio: "bio" };
+var ww = Loco.gui.create( "WebWindow" );
+ww.setHtml( Haml.render( Loco.ctx.read( "test25-haml.haml" ) ) );
+ww.show();
+
+//==============================================================================
+//exit(0); //FOR NON-GUI APPS ONLY
+
+} catch( e ) {
+  if( e.message ) Loco.console.printerrln( e.message );
+  else Loco.console.printerrln( e );
+  exit( -1 );
+}
+
+
+
