39ca541f7e88d6a8980a0c8e467dc220ffcf91f6
uvaretto <uvaretto@ugo-workstation.(none)>
2012-01-02 11:48:30 +0100

    Added license and readme

---
+++ etc/qlua/LuaCallbackDispatcher.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <QMap>
+#include <QList>
+#include <QMetaType>
+
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+
+namespace qlua {
+
+class LuaContext;
+
+typedef QList< QMetaType::Type > ParameterTypes;
+
+void PushLuaValue( LuaContext* lc, QMetaType::Type type, void* arg );
+
+//------------------------------------------------------------------------------
+class LuaCBackMethod {
+public:
+    LuaCBackMethod( LuaContext* lc, const ParameterTypes& p, int luaCBackRef ) 
+        : lc_( lc ), paramTypes_( p ), luaCBackRef_( luaCBackRef ) {}
+    void Invoke( void **arguments );
+    int CBackRef() const{ return luaCBackRef_; } 
+private:
+    LuaContext* lc_;
+    ParameterTypes paramTypes_;
+    int luaCBackRef_;
+};
+
+
+typedef int LuaCBackRef;
+typedef int MethodId;
+
+//------------------------------------------------------------------------------
+class LuaCallbackDispatcher : public QObject {
+public:
+    LuaCallbackDispatcher( QObject* parent = 0 ) : QObject( parent ), lc_( 0 ) {}
+    LuaCallbackDispatcher( LuaContext* lc ) : lc_( lc ) {}
+    int qt_metacall( QMetaObject::Call c, int id, void **arguments ); 
+    bool Connect( QObject *obj, 
+                  int signalIdx,
+                  const QList< QMetaType::Type >& paramTypes,
+                  int luaCBackRef );
+    bool Disconnect( QObject *obj, 
+                     int signalIdx,
+                     int cbackStackIndex );
+    void SetLuaContext( LuaContext* lc ) { lc_ = lc; };
+    virtual ~LuaCallbackDispatcher() {
+        for( QList< LuaCBackMethod* >::iterator i = luaCBackMethods_.begin();
+             i != luaCBackMethods_.end(); ++i ) {
+            delete *i;
+        }
+    }
+private:
+    LuaContext* lc_;
+    QList< LuaCBackMethod* > luaCBackMethods_;
+    QMap< LuaCBackRef, MethodId > cbackToMethodIndex_;
+   
+};
+}
---
+++ etc/qlua/test/qlua-test.cpp
@@ -0,0 +1,46 @@
+#include <cassert>
+#include <QPointer>
+#include <iostream>
+#include "../LuaContext.h"
+
+#include "TestObject.h"
+
+//------------------------------------------------------------------------------
+int main() {
+    try {
+        qlua::LuaContext ctx;
+        
+        TestObject myobj;
+        myobj.setObjectName( "MyObject" );
+        //only add a single method to the Lua table
+        ctx.AddQObject( &myobj, "myobj", qlua::LuaContext::QOBJ_NO_DELETE, QStringList() << "emitSignal" );
+        ctx.Eval( "qlua.connect( myobj, 'aSignal()', function() print( 'Lua callback called!' ); end )" );
+        ctx.Eval( "print( 'object name: ' .. myobj.objectName )" );
+        //both the following lines work
+        //ctx.Eval( "qtconnect( myobj, 'aSignal()', myobj.qobject__, 'aSlot()' )" );
+        ctx.Eval( "qlua.connect( myobj, 'aSignal()', myobj, 'aSlot()' )" );
+        ctx.Eval( "myobj.emitSignal()" );
+
+        TestObject* myobj2 = new TestObject;
+        QPointer< TestObject > pMyObject = myobj2;
+        myobj2->setObjectName( "MyObject2" );
+        ctx.AddQObject( myobj2, "myobj2", qlua::LuaContext::QOBJ_IMMEDIATE_DELETE );
+        ctx.Eval( "print( 'object 2 name: '..myobj2.objectName )" );
+        ctx.Eval( "getmetatable( myobj2 ).__gc()" );
+        assert( pMyObject.isNull() );
+        
+        TestObject myobj3;
+        ctx.AddQObject( &myobj3, "myobj3", qlua::LuaContext::QOBJ_NO_DELETE );
+        ctx.Eval( "print( myobj3.copyString( 'hi' ) );\n"
+                  "vm = myobj3.copyVariantMap( {key1=1,key2='hello'} );\n" 
+                  "print( vm['key1'] .. ' ' .. vm['key2'] );\n"
+                  "print( myobj3.createObject().objectName );" );
+
+        ctx.Eval( "fl = myobj3.copyShortList( {1,2,3} );\n" 
+                  "print( fl[1] .. ' ' .. fl[ 3 ] );\n" );
+
+    } catch( const std::exception& e ) {
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
+}
