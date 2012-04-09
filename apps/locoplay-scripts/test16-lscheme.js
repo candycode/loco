ded88c4e1ae44aa557750d98b8cbf591f914b1b0
ugo <ugovaretto@gmail.com>
2012-04-09 23:35:49 +0200

    Partially fixed issue #6 by removing file evaluation test

--- apps/locoplay-scripts/test16-lscheme.js
+++ apps/locoplay-scripts/test16-lscheme.js
@@ -1,14 +1,15 @@
-//var print = Loco.console.println;
+var print = Loco.console.println;
 var WKIT = Loco.ctx.jsInterpreterName().indexOf( "webkit" ) >= 0;
 var read = Loco.ctx.read;
 var exit = Loco.ctx.exit;
 
 try { 
-  if( WKIT ) Loco.ctx.include( "../filters/lscheme/little.js" );
-  else eval( read( "../filters/lscheme/little.js" ) );
-  p(value(s("(print (quote(ciao) ) )") ) );
-  Loco.ctx.addScriptFilter( "lscheme", "value(s($))", "", "", "$" );
-  Loco.ctx.evalFile( "test16.lscheme", ["lscheme"] );
+  if( WKIT ) {
+    Loco.ctx.include( "../../filters/lscheme/little.js" );
+    print = alert;
+  }
+  else eval( read( "../../filters/lscheme/little.js" ) );
+  p(value(s( "(print ( quote( ciao)  ) )") ) );
   exit( 0 );
 } catch( e ) {
   print( e );
