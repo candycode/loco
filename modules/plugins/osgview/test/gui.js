39c1b53f0d9429738159fa6c267fc375c7711bce
ugo <ugovaretto@gmail.com>
2012-04-30 22:57:25 +0200

    Added knockoutjs code to osgviewer

--- modules/plugins/osgview/test/gui.js
+++ modules/plugins/osgview/test/gui.js
@@ -13,10 +13,14 @@ var l = Loco.gui.create( "HBoxLayout" );
 l.addWidget( osgview );
 l.setContentsMargins( 0, 0, 0, 0 );
 mw.setLayout( l );
-osgview.loadPage( "./gui.html", true );
 osgview.setContinuousUpdate( 20 );
 osgview.addJSObject( "gui", Loco.gui );
 osgview.addJSObject( "win", mw );
+osgview.addJSObject( "ctx", Loco.ctx );
+osgview.jsinit = "eval(ctx.read('jquery.js')); \
+                  eval(ctx.read('jquery-ui.js')); \
+                  eval(ctx.read('knockout.js'));";
+osgview.loadPage( "./gui.html", true );
 if( Loco.ctx.args.length > 2 ) {
   osgview.loadScene( Loco.ctx.args[ 2 ] );
 } else osgview.loadScene( "dumptruck.osg" );
