20d1c4797468b894c795b54afe73ff3039e8ede5
ugo <ugovaretto@gmail.com>
2012-05-25 12:49:37 +0300

    Update libloco/include/Arrays/DynamicAlignedAllocator.h

--- libloco/include/Arrays/DynamicAlignedAllocator.h
+++ libloco/include/Arrays/DynamicAlignedAllocator.h
@@ -8,7 +8,7 @@
 
 ///\warning INTENDED FOR POD TYPES ONLY
 /// The only changes required to make it work with any type
-/// are to explicitly invoke destructors in the deallocate() abd destroy() methods
+/// are to explicitly invoke destructors in the deallocate() and destroy() methods
 /// and use placement new in construct()
 
 template <class T > class DynamicAlignedAllocator {
