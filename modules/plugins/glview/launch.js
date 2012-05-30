50feb1143ddd5058f56f435dcc05806f18c5c435
ugo <uv@cyberhipster.net>
2012-05-30 14:18:03 +0200

    Fixed plugin path

--- modules/plugins/glview/launch.js
+++ modules/plugins/glview/launch.js
@@ -1,11 +1,11 @@
 try {
 var ppath = "";
 if( Loco.ctx.os() === "WINDOWS" ) 
-  ppath = "/cmakebuilds/project-loco/modules/plugins/osgview/Release/glview.dll";
+  ppath = "/cmakebuilds/project-loco/modules/plugins/glview/Release/glview.dll";
 else
-  ppath = Loco.ctx.homeDir() + "/build/loco/modules/plugins/osgview/libglview.so";
+  ppath = Loco.ctx.homeDir() + "/build/loco/modules/plugins/glview/libglview.so";
 var mw = Loco.gui.create("MainWindow");
-mw.setCentralWidget( Loco.ctx.loadQtPlugin(ppath);
+mw.setCentralWidget( Loco.ctx.loadQtPlugin(ppath));
 mw.show();
 } catch(e){
   Loco.console.printerrln( e.message || e );
